1dd91d7cb5748199711af14ae2e74ec9ae5b5d79
Matthew Parker <matthewparker@hotmail.com.au>
2023-07-29 22:12:24 +1000

    Blog class

---
+++ src/MattParkerDev.WebUI/Services/BlogService.cs
@@ -0,0 +1,26 @@
+﻿using MattParkerDev.WebUI.Models;
+
+namespace MattParkerDev.WebUI.Services;
+
+public sealed class BlogService
+{
+    public static readonly List<Blog> Blogs = new List<Blog>
+    {
+        new Blog()
+        {
+            Sequence = 0,
+            Slug = "my-first-blog",
+            Title = "My First Blog!",
+            Description =
+                "This is my first blog. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut.",
+            Content = "This is my first blog. It's not very good."
+        }
+    };
+
+    public static Blog? GetBlogBySlug(string slug)
+    {
+        return Blogs.FirstOrDefault(b => b.Slug == slug);
+    }
+
+    public static List<string> SlugList => Blogs.Select(b => b.Slug).ToList();
+}
