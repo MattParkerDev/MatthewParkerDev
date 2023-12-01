6d523f65df8a2609c89116dd4fb55d65ad8622bf
Matthew Parker <matthewparker@hotmail.com.au>
2023-12-01 23:20:24 +1000

    style

--- src/MattParkerDev.WebUI/Properties/tailwind.config.js
+++ src/MattParkerDev.WebUI/Properties/tailwind.config.js
@@ -7,6 +7,26 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    colors: {
+      'text': '#000000',
+      'background': '#fafafa',
+      'primary': '#bd9ddd',
+      'secondary': '#dad7ea',
+      'accent': '#9edcad',
+      'gray': {
+        50: '#f2f2f2',
+        100: '#e6e6e6',
+        200: '#cccccc',
+        300: '#b3b3b3',
+        400: '#999999',
+        500: '#808080',
+        600: '#666666',
+        700: '#4d4d4d',
+        800: '#333333',
+        900: '#1a1a1a',
+        950: '#0d0d0d',
+      },
+    },
   },
   plugins: [],
 }
