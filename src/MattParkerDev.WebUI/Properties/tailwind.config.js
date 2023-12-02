6fe627e50e092c651f5462496da95d1282b29344
Matthew Parker [SSW] <61717342+MattParkerDev@users.noreply.github.com>
2023-12-02 23:41:58 +1000

    styling v1.4

--- src/MattParkerDev.WebUI/Properties/tailwind.config.js
+++ src/MattParkerDev.WebUI/Properties/tailwind.config.js
@@ -7,6 +7,16 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '8rem',
+        '2xl': '10rem',
+      }
+    },
     colors: {
       'text': '#000000',
       'background': '#fafafa',
