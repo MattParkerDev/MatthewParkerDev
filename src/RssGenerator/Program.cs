183e02b71194da659f5a653cb5b866b0f4bb621e
Matthew Parker [SSW] <61717342+MattParkerDev@users.noreply.github.com>
2024-05-14 14:38:12 +1000

    add xml feed

---
+++ src/RssGenerator/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.ServiceModel.Syndication;
+using System.Xml;
+using MattParkerDev.WebUI.Services;
+
+Console.WriteLine("Generating RSS feed...");
+
+XmlWriter writer = XmlWriter.Create("../../../../src/MattParkerDev.WebUI/wwwroot/feed.xml");
+
+var items = BlogService.Blogs.Select(post => new SyndicationItem(
+    post.Title, 
+    post.Description, 
+    new Uri("https://mattparker.dev/blog/" + post.Slug)
+    )
+    {
+        Title = new TextSyndicationContent(post.Title),
+        Content = new TextSyndicationContent(post.Description),
+        PublishDate = post.PublishedDate,
+        Authors = { new SyndicationPerson(null, "Matt Parker", "https://mattparker.dev") },
+    }
+).ToList();
+
+var feed = new SyndicationFeed(items)
+{
+    Title = new TextSyndicationContent("MattParkerDev's Blog"),
+    Description = new TextSyndicationContent("My portfolio website and where I blog about .NET, Azure and all things software."),
+    LastUpdatedTime = DateTimeOffset.Now
+};
+
+feed.SaveAsRss20(writer);
+
+writer.Close();
+
+Console.WriteLine("RSS feed generated.");
